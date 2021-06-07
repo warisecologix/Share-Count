0343ac60b3394ecacde471939796fbb77275923a
Tahir_Work <you@example.com>
2021-06-07 22:49:57 +0500

    email

--- app/Http/Controllers/AJAXController.php
+++ app/Http/Controllers/AJAXController.php
@@ -52,7 +52,24 @@ class AJAXController extends Controller
 
         $session_id = Session::getId();
         $random_number = mt_rand(1000, 9999);
-        Mail::to($request->email)->send(new VerifyUser($random_number));
+
+
+        //Mail::to($request->email)->send(new VerifyUser($random_number));
+        $from = "verification@trackshortage.com";
+        $to = $request->email;
+        $subject = "Checking PHP mail";
+        $message = "This is your OTP Code" . $random_number;
+        $headers = "Reply-To: Count <verification@trackshortage.com>\r\n";
+        $headers .= "Return-Path: The Sender <verification@trackshortage.com>\r\n";
+        $headers .= "From: The Sender <verification@trackshortage.com>\r\n";
+        $headers .= "Organization: Sender Organization\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+        $headers .= "X-Priority: 3\r\n";
+        $headers .= "X-Mailer: PHP" . phpversion() . "\r\n";
+        mail($to, $subject, $message, $headers);
+
+
         $collection = EmailVerify::where('session_id', $session_id)->where('type' , 0)->get();
         foreach ($collection as $c) {
             $c->delete();
