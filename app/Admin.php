95ca43f2b150d3f5dc7ed0308135636ee884c212
mwariszargar <muhammad.waris@ecologix.us>
2021-06-11 16:39:07 +0500

    Add relation i n admin

--- app/Admin.php
+++ app/Admin.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Admin extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function stock(){
+        return $this->belongsTo(Stock::class);
+    }
 }
