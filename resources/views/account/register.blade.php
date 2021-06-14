ce000c0020bea8292bb020ece72842cce7d00432
mwariszargar <muhammad.waris@ecologix.us>
2021-06-14 17:34:23 +0500

    Phone fname & lname redesign

--- resources/views/account/register.blade.php
+++ resources/views/account/register.blade.php
@@ -117,73 +117,36 @@
                                     <input type="hidden" value="0" name="user_exists" id="user_exists">
 
                                     <div class="form-group row">
-                                        <label for="first_name"
-                                               class="col-md-4 col-form-label text-md-right">{{ __('First Name') }}</label>
-
-                                        <div class="col-md-6">
-                                            <input id="first_name" type="text"
-                                                   class="form-control "
-                                                   placeholder="Enter first name"
-                                                   name="first_name" value="{{ old('first_name') }}"
-                                                   autocomplete="first_name">
-
+                                        <div class="col-sm-6">
+                                            <label for="fname">First name</label>
+                                            <input id="first_name" type="text" class="form-control " placeholder="Enter first name" name="first_name" value="{{ old('first_name') }}">
                                         </div>
-                                    </div>
-
-                                    <div class="form-group row">
-                                        <label for="last_name"
-                                               class="col-md-4 col-form-label text-md-right">{{ __('Last Name') }}</label>
-
-                                        <div class="col-md-6">
-                                            <input id="last_name" type="text"
-                                                   class="form-control"
-                                                   placeholder="Enter last name"
-                                                   name="last_name" value="{{ old('last_name') }}"
-                                                   autocomplete="last_name">
-
+                                        <div class="col-sm-6">
+                                            <label for="lname">Last name</label>
+                                            <input id="last_name" type="text" class="form-control" placeholder="Enter last name" name="last_name" value="{{ old('last_name') }}">
                                         </div>
                                     </div>
 
                                     <div class="form-group row" id="div_phone_number">
-                                        <label for="email"
-                                               class="col-md-4 col-form-label text-md-right">{{ __('Phone Number') }}</label>
-                                        <div class="col-md-6">
-                                            <div class="input-group mb-3">
-                                                <input id="phone_no" type="number"
-                                                       class="form-control "
-                                                       name="phone_no"
-                                                       placeholder="Enter valid phone no"
-                                                       value="{{ old('phone_no') }}" autocomplete="phone_no" autofocus>
-
-                                                <div class="input-group-append"
-                                                     id="phone_number_send_verify_code_verify">
-                                                    <button type="button" id="phone_number_send_verify_code"
-                                                            class="btn btn-primary input-group-text">Verify number
-                                                    </button>
-                                                </div>
-                                            </div>
+                                        <div class="col-sm-10" id="div_phone_number_field">
+                                            <label for="phone_no">Phone Number</label>
+                                            <br/>
+                                            <input type="number" class="form-control" id="phone_no" name="phone_no" placeholder="Enter valid phone no" value="{{ old('phone_no') }}">
+                                        </div>
+                                        <div class="col-sm-2">
+                                            <button type="button" id="phone_number_send_verify_code" class="btn btn-top-margin custom-button">Verify</button>
                                         </div>
                                     </div>
 
-                                    <div id="div_phone_number_verification" class="div-hidden">
-                                        <div class="form-group row" id="div_phone_number">
-                                            <label for="verify_phone_number_code"
-                                                   class="col-md-4 col-form-label text-md-right">{{ __('Verification Code') }}</label>
-                                            <div class="col-md-6">
-                                                <input id="verify_phone_number_code" type="number"
-                                                       class="form-control bg-info "
-                                                       name="verify_phone_number_code"
-                                                       placeholder="Enter verification code"
-                                                       value="{{ old('verify_phone_number_code') }}"
-                                                       autocomplete="verify_phone_number_code">
-                                            </div>
+                                    <div class="form-group row div-hidden" id="div_phone_number_verification">
+                                        <div class="col-sm-12">
+                                            <label for="verification_code">Verification Code</label>
+                                            <input id="verify_phone_number_code" type="number" class="form-control " name="verify_phone_number_code" placeholder="Enter verification code" value="{{ old('verify_phone_number_code') }}" autocomplete="verify_phone_number_code">
                                         </div>
-                                        <div class="form-group row mb-0 text-right">
-                                            <div class="col-md-10 mb-3">
-                                                <button id="verify_phone_otp" class="btn btn-primary">
-                                                    {{ __('Verify One Time Passcode') }}
-                                                </button>
-                                            </div>
+                                        <div class="col-md-4 offset-8 mt-3 text-right">
+                                            <button id="verify_phone_otp" class="btn custom-button">
+                                                {{ __('Verify One Time Passcode') }}
+                                            </button>
                                         </div>
                                     </div>
 
@@ -571,6 +534,46 @@
                     </div>
                 </div>
             </form>
+        </div>
+    </div>
+
+    <div class="container">
+        <div id="footer">
+            <div class="row">
+                <div class="col-12">
+                    <hr class="company-hr" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4 footer-text">For educational purposes only</div>
+                <div class="col-5 offset-3">
+                    <div class="row">
+                        <div class="col-4">
+                            <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M29.4875 4.34421C29.1692 3.46878 28.6628 2.67375 28.0041 2.01507C27.3455 1.35639 26.5504 0.850061 25.675 0.531714C24.0375 0.0067139 22.55 0.00671387 19.525 0.00671387H10.45C7.45 0.00671387 5.9375 0.00671387 4.325 0.506714C3.4483 0.826387 2.65247 1.33463 1.9937 1.99556C1.33494 2.65648 0.829302 3.45397 0.5125 4.33171C0 5.95671 0 7.45671 0 10.4567V19.5442C0 22.5567 0 24.0442 0.5 25.6692C0.818347 26.5446 1.32468 27.3397 1.98336 27.9984C2.64204 28.657 3.43707 29.1634 4.3125 29.4817C5.9375 29.9942 7.4375 29.9942 10.4375 29.9942H19.5375C22.55 29.9942 24.05 29.9942 25.6625 29.4942C26.5415 29.1761 27.3398 28.6685 28.0008 28.0075C28.6618 27.3465 29.1694 26.5482 29.4875 25.6692C30 24.0442 30 22.5442 30 19.5442V10.4817C30 7.46921 30 5.96921 29.4875 4.34421ZM21.775 10.1317L20.6125 11.2942C20.4992 11.3996 20.3509 11.4592 20.1962 11.4615C20.0414 11.4638 19.8914 11.4087 19.775 11.3067C18.6492 10.3559 17.2236 9.83351 15.75 9.83171C14.5375 9.83171 13.325 10.2317 13.325 11.3442C13.325 12.4692 14.625 12.8442 16.125 13.4067C18.75 14.2817 20.925 15.3817 20.925 17.9567C20.925 20.7567 18.75 22.6817 15.2 22.8942L14.875 24.3942C14.8463 24.5321 14.7709 24.6558 14.6616 24.7446C14.5523 24.8334 14.4158 24.8818 14.275 24.8817H12.0375L11.925 24.8692C11.7648 24.8333 11.6253 24.7358 11.5364 24.5978C11.4475 24.4598 11.4165 24.2924 11.45 24.1317L11.8 22.5442C10.4528 22.2065 9.2138 21.5311 8.2 20.5817V20.5692C8.14406 20.5135 8.09968 20.4472 8.0694 20.3743C8.03911 20.3014 8.02353 20.2232 8.02353 20.1442C8.02353 20.0652 8.03911 19.9871 8.0694 19.9141C8.09968 19.8412 8.14406 19.775 8.2 19.7192L9.45 18.5067C9.56351 18.4004 9.71322 18.3412 9.86875 18.3412C10.0243 18.3412 10.174 18.4004 10.2875 18.5067C11.425 19.5817 12.95 20.1817 14.525 20.1567C16.15 20.1567 17.2375 19.4692 17.2375 18.3817C17.2375 17.2942 16.1375 17.0067 14.0625 16.2317C11.8625 15.4442 9.775 14.3317 9.775 11.7317C9.775 8.70671 12.2875 7.23171 15.2625 7.09421L15.575 5.55671C15.6041 5.42007 15.6799 5.29785 15.7894 5.21114C15.899 5.12443 16.0353 5.07866 16.175 5.08171H18.4L18.525 5.09421C18.85 5.16921 19.0625 5.48171 18.9875 5.80671L18.65 7.51921C19.775 7.89421 20.8375 8.48171 21.75 9.25671L21.775 9.28171C22.0125 9.53171 22.0125 9.90671 21.775 10.1317Z"
+                                      fill="#00C244"/>
+                            </svg>
+                            <span class="social-media-text">Cash App</span>
+                        </div>
+                        <div class="col-4">
+                            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M30.0533 8.56004C29.8949 7.92725 29.5723 7.34747 29.1182 6.87925C28.664 6.41103 28.0943 6.07095 27.4666 5.89337C25.1733 5.33337 15.9999 5.33337 15.9999 5.33337C15.9999 5.33337 6.82662 5.33337 4.53328 5.94671C3.90561 6.12429 3.33592 6.46436 2.88174 6.93258C2.42757 7.4008 2.105 7.98059 1.94662 8.61337C1.5269 10.9408 1.3216 13.3018 1.33328 15.6667C1.31832 18.0494 1.52364 20.4284 1.94662 22.7734C2.12123 23.3865 2.45102 23.9442 2.90414 24.3927C3.35726 24.8411 3.91838 25.1651 4.53328 25.3334C6.82662 25.9467 15.9999 25.9467 15.9999 25.9467C15.9999 25.9467 25.1733 25.9467 27.4666 25.3334C28.0943 25.1558 28.664 24.8157 29.1182 24.3475C29.5723 23.8793 29.8949 23.2995 30.0533 22.6667C30.4698 20.3568 30.675 18.0138 30.6666 15.6667C30.6816 13.284 30.4763 10.905 30.0533 8.56004V8.56004Z"
+                                      stroke="#FF0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M13 20.0266L20.6667 15.6666L13 11.3066V20.0266Z" stroke="#FF0000" stroke-width="2"
+                                      stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <span class="social-media-text">YouTube</span>
+                        </div>
+                        <div class="col-4">
+                            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M30.6666 3.99998C29.3898 4.90062 27.9761 5.58946 26.48 6.03998C25.677 5.11666 24.6098 4.46224 23.4227 4.16521C22.2356 3.86819 20.986 3.94291 19.8428 4.37925C18.6995 4.81559 17.7179 5.59252 17.0306 6.60494C16.3433 7.61736 15.9836 8.81643 16 10.04V11.3733C13.6568 11.4341 11.335 10.9144 9.24132 9.86057C7.14764 8.80675 5.34707 7.25149 3.99998 5.33331C3.99998 5.33331 -1.33335 17.3333 10.6666 22.6666C7.92068 24.5306 4.64952 25.4652 1.33331 25.3333C13.3333 32 28 25.3333 28 9.99998C27.9988 9.62858 27.963 9.25811 27.8933 8.89331C29.2541 7.5513 30.2144 5.85693 30.6666 3.99998V3.99998Z"
+                                      stroke="#1DA1F2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <span class="social-media-text">Twitter</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
