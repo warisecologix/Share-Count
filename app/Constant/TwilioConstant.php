2a1fa6462b81ec56033ea07e377f18796a6d1c36
mwariszargar <muhammad.waris@ecologix.us>
2021-06-19 17:43:49 +0500

    Twillio token update

--- app/Constant/TwilioConstant.php
+++ app/Constant/TwilioConstant.php
@@ -5,7 +5,7 @@ namespace App\Constant;
 class TwilioConstant
 {
     const TWILIO_SID = "ACe1a8d38c382c1d74697166d2d9708ae1";
-    const TWILIO_AUTH_TOKEN = "b1e6b427e81f31928f9ee58426445110";
+    const TWILIO_AUTH_TOKEN = "62cba9efd6241a509b86fb8bc8ef4c3b";
     const TWILIO_VERIFY_SID = "VA9ea2a5b0934ee1d29d2a2c11efc64d1e";
     const TWILIO_NUMBER = "+15102964187";
     const TWILIO_VERIFY_NUMBER = "+92 323 4812995";
